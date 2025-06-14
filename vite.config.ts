d5c03b5d7758a2de0af08526e41204090252f0de
a0ka1 <support@rameego.com>
2025-06-14 11:07:00 +0300

    updated config and packages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Attendance/',
+  base: '/attendancer/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
